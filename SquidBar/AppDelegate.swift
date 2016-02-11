06a69a8d5bcef5bc9ba2f39b3b24b5005ecd1f53
Robert Burns <burnsra@gmail.com>
2016-02-11 18:52:15 -0500

    Initial Commit

---
+++ SquidBar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SquidBar
+//
+//  Created by Robert Burns on 2/11/16.
+//  Copyright © 2016 Robert Burns. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
